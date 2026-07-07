65ac88b8d5f540914080b7ce62d8c85036c6d2ae
Nadya Adelya <250212030@student.ar-raniry.ac.id>
2026-07-07 20:18:52 +0700

    hapus if, else koneksi

--- koneksi.php
+++ koneksi.php
@@ -6,9 +6,5 @@ $password = "";
 $database = "db_tugas_mahasiswa";
 
 $koneksi = mysqli_connect($host, $username, $password, $database);
-if ($koneksi) {
-    echo "database terkoneksi: ";
-}else {
-    echo "database tidak terkoneksi: ";
-}
+
 ?>
