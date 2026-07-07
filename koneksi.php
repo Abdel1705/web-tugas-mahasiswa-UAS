37257cac042cd367eca1c112544eb7aee1896506
Nadya Adelya <250212030@student.ar-raniry.ac.id>
2026-07-07 20:50:47 +0700

    menghapus if dan else koneksi

--- koneksi.php
+++ koneksi.php
@@ -6,9 +6,5 @@ $password = "";
 $database = "db_tugas_mahasiswa";
 
 $koneksi = mysqli_connect($host, $username, $password, $database);
-if ($koneksi) {
-    echo "Alhamdulillah, Php sudah konek" . $database;
-} else {
-    die ("koneksi gagal ey" . mysqli_connect_error());
-}
+
 ?>
