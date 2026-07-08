73389c1b43ff0a0b3d75c5866138767269745b13
Zaharatul Ulya <ulyazaharatul30@gmail.com>
2026-07-08 13:52:00 +0700

    php koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,6 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "db_tugas_mahasiswa.sql";
+$database = "db_tugas_mahasiswa";
 $koneksi = mysqli_connect($host, $username, $password, $database);
 ?>
