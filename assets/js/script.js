47047ae3bcd36a105a5752377bbd6ceb38d670b6
Nuzuliya Saffana <250212001@student.ar-raniry.ac.id>
2026-07-08 14:43:23 +0700

    hapus animasi

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const heroTitle = document.querySelector(".hero-title");
-
-window.addEventListener("load", function () {
-    setTimeout(function () {
-        heroTitle.classList.add("show");
-    }, 300);
-});
